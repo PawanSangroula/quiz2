779227591414d4e62661aa39bc9c0b5ac1c25203
mbista-debug <mbista@algomau.ca>
2026-02-03 17:14:18 -0500

    made chages on studytip

--- src/app/features/StudyTipFeature.java
+++ src/app/features/StudyTipFeature.java
@@ -4,10 +4,10 @@ public class StudyTipFeature {
     public static String run() {
         return "Study Tip\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Study Style: YOUR STUDY STYLE HERE\n"
+             + "Student Name: Mausam Bista\n"
+             + "Study Style: Visual Learner\n"
              + "Tip:\n"
-             + "Replace this text with your own study tip.\n"
+             + "Use diagrams and mind maps to visualize concepts and relationships.\n"
              + "------------------------------\n";
     }
 }
