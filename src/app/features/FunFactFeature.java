4edb4abfb7b7dcd3917649b15afedb81c6583338
PawanSangroula <spawansang@gmail.com>
2026-02-03 16:14:16 -0500

    made chages on funfact

--- src/app/features/FunFactFeature.java
+++ src/app/features/FunFactFeature.java
@@ -4,9 +4,9 @@ public class FunFactFeature {
     public static String run() {
         return "Fun Fact\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
+             + "Student Name: Paban Sangroula\n"
              + "Fun Fact About You:\n"
-             + "Replace this text with a fun fact.\n"
+             + "I can sleep 24 hours straight\n"
              + "------------------------------\n";
     }
 }
