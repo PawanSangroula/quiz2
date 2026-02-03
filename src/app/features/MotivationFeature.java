dae09566983fed8be4c69fe01ec03a590e2d16f2
coadingdarsh <rushtodarshal@gmail.com>
2026-02-03 16:49:49 -0500

    made chages in motication

--- src/app/features/MotivationFeature.java
+++ src/app/features/MotivationFeature.java
@@ -4,9 +4,9 @@ public class MotivationFeature {
     public static String run() {
         return "Motivation\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "What motivates you:\n"
-             + "Replace this text with your motivation.\n"
+             + "Student Name: YOUR NAME Darshal Thumar\n"
+             + "What motivates you: need a heart\n"
+             + " money motivation.\n"
              + "------------------------------\n";
     }
 }
